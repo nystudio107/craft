04eee0270acb52a146c0eed37a28a9c79dfbb11e
Andrew Welch <andrew@nystudio107.com>
2017-11-28 15:06:57 -0500

    Add support for Redis
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- config/app.php
+++ config/app.php
@@ -14,8 +14,84 @@
  */
 
 return [
-    'modules' => [
-        'my-module' => \modules\Module::class,
+
+    // All environments
+    '*' => [
+        'modules' => [
+            'my-module' => \modules\Module::class,
+        ],
+        //'bootstrap' => ['my-module'],
     ],
-    //'bootstrap' => ['my-module'],
+
+    // Live (production) environment
+    'live'  => [
+        'components' => [
+            // Default to database 0, so PHP sessions are in a separate database
+            'redis' => [
+                'class' => yii\redis\Connection::class,
+                'hostname' => 'localhost',
+                'port' => 6379,
+                'database' => 0,
+            ],
+            'cache' => [
+                // Use database 1 for live production
+                'class' => yii\redis\Cache::class,
+                'redis' => [
+                    'hostname' => 'localhost',
+                    'port' => 6379,
+                    'database' => 1,
+                ],
+            ],
+            'session' => function() {
+                $stateKeyPrefix = md5('Craft.'.craft\web\Session::class.'.'.Craft::$app->id);
+                /** @var yii\redis\Session $session */
+                $session = Craft::createObject([
+                    'class' => yii\redis\Session::class,
+                    'flashParam' => $stateKeyPrefix.'__flash',
+                    'name' => Craft::$app->getConfig()->getGeneral()->phpSessionName,
+                    'cookieParams' => Craft::cookieConfig(),
+                ]);
+                $session->attachBehaviors([craft\behaviors\SessionBehavior::class]);
+                return $session;
+            },
+        ],
+    ],
+
+    // Staging (pre-production) environment
+    'staging'  => [
+        // Default to database 0, so PHP sessions are in a separate database
+        'components' => [
+            'redis' => [
+                'class' => yii\redis\Connection::class,
+                'hostname' => 'localhost',
+                'port' => 6379,
+                'database' => 0,
+            ],
+            // Use database 2 for staging
+            'cache' => [
+                'class' => yii\redis\Cache::class,
+                'redis' => [
+                    'hostname' => 'localhost',
+                    'port' => 6379,
+                    'database' => 2,
+                ],
+            ],
+            'session' => function() {
+                $stateKeyPrefix = md5('Craft.'.craft\web\Session::class.'.'.Craft::$app->id);
+                /** @var yii\redis\Session $session */
+                $session = Craft::createObject([
+                    'class' => yii\redis\Session::class,
+                    'flashParam' => $stateKeyPrefix.'__flash',
+                    'name' => Craft::$app->getConfig()->getGeneral()->phpSessionName,
+                    'cookieParams' => Craft::cookieConfig(),
+                ]);
+                $session->attachBehaviors([craft\behaviors\SessionBehavior::class]);
+                return $session;
+            },
+        ],
+    ],
+
+    // Local (development) environment
+    'local'  => [
+    ],
 ];
