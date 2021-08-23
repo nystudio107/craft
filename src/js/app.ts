cb29de6a2c5f7e8cb4de4ae0f8345b8a525d389b
Cavell Blood <cavellblood@gmail.com>
2021-08-23 15:40:46 -0500

    Add code for test case

--- src/js/app.ts
+++ src/js/app.ts
@@ -4,6 +4,25 @@ import { createApp } from 'vue';
 // Import our CSS
 import '@/css/app.pcss';
 
+import $ from 'jquery';
+import Flickity from 'flickity';
+
+// Import flickity styles for gallery
+import 'flickity/css/flickity.css';
+
+// Test if jQuery is loaded.
+console.log($);
+$('#test-container').html('It works from app.ts from /src/js');
+
+
+// Intialize galleries https://flickity.metafizzy.co/#initialize-with-vanilla-javascript
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
 // App main
 const main = async () => {
     // Create our vue instance
