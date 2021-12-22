d5bd0273d0f05b70529ab4a4aceb47fc928bfbca
Andrew Welch <andrew@keluli.local>
2021-12-22 13:52:39 -0500

    refactor: Updated to use a unified `bootstrap.php`

--- cms/web/index.php
+++ cms/web/index.php
@@ -3,19 +3,10 @@
  * Craft web bootstrap file
  */
 
-// Set path constants
-define('CRAFT_BASE_PATH', dirname(__DIR__));
-define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
-
-// Load Composer's autoloader
-require_once CRAFT_VENDOR_PATH.'/autoload.php';
-
-// Load dotenv?
-if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH.'/.env')) {
-    Dotenv\Dotenv::create(CRAFT_BASE_PATH)->load();
-}
+// Load shared bootstrap
+require dirname(__DIR__) . '/bootstrap.php';
 
 // Load and run Craft
-define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
-$app = require CRAFT_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
+/** @var craft\web\Application $app */
+$app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
 $app->run();
