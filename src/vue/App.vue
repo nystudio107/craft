b3577d98d99948923b9096725152b36aa33391be
Andrew Welch <andrew@nystudio107.com>
2022-01-14 09:36:32 -0500

    refactor: Switch to `<script setup>`

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,20 +1,7 @@
 <template>
-    <confetti></confetti>
+  <confetti-party />
 </template>
 
-<script lang="ts">
-// Async load the Vue 3 APIs we need from the Vue ESM
-import {defineAsyncComponent, defineComponent } from 'vue';
-import Confetti from '@/vue/Confetti.vue';
-export default defineComponent({
-  components: {
-    'confetti': Confetti,
-  },
-  data: () => ({
-  }),
-  methods: {
-  },
-  mounted() {
-  },
-});
+<script setup lang="ts">
+import ConfettiParty from '@/vue/ConfettiParty.vue';
 </script>
