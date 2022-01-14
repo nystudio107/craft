2a08faa3dc67c0aea3593dbb94b808d07c15ccfd
Andrew Welch <andrew@keluli.local>
2022-01-14 18:29:03 -0500

    refactor: .pcss -> .css & eslint cleanup

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,19 +1,19 @@
 import App from '@/vue/App.vue';
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 
 // Import our CSS
-import '@/css/app.pcss';
+import '@/css/app.css';
 
 // App main
 const main = async () => {
     // Create our vue instance
     const app = createApp(App);
-    
+
     // Mount the app
     return app.mount('#component-container');
 };
 
 // Execute async function
-main().then( () => {
+main().then(() => {
     console.log();
 });
